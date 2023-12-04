97f77762874116fb942b0a6ac41ce920fd5a43a7
rockthepeople <3qufqks@gmail.com>
2023-12-05 02:04:28 +0900

    Update : 투두 디자인(1)

--- src/Presentation/Component/todo.tsx
+++ src/Presentation/Component/todo.tsx
@@ -5,50 +5,50 @@ import Button from "./button";
 
 const Todo = ({ label, name, todoType, prevValue, checked, startTime, endTime }: TodoProps) => {
 
-    const calculateFillPercentage = () => { if(sliderValue !== undefined) {return (sliderValue/ 100) * 100; }};
+    const calculateFillPercentage = () => { if (sliderValue !== undefined) { return (sliderValue / 100) * 100; } };
     const [sliderValue, setSliderValue] = useState(prevValue);
     const handleSliderChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-        const newValue = parseInt(e.target.value, 10); 
-        setSliderValue(newValue); 
+        const newValue = parseInt(e.target.value, 10);
+        setSliderValue(newValue);
     };
     // isTodoCheck ===true, make toggle button 
     const [isDone, setIsDone] = useState(checked);
-    const setDone = (boo:boolean) => {
+    const setDone = (boo: boolean) => {
         setIsDone(boo => !boo);
     }
-    const handleDone: ChangeEventHandler<HTMLInputElement> = (e) => {setDone(e.target.checked)}
-    
+    const handleDone: ChangeEventHandler<HTMLInputElement> = (e) => { setDone(e.target.checked) }
+
     return (
         <div className={style.TodoBox}>
+            <label>
+                <p>{startTime}</p>
+                <p>{endTime}</p>
+            </label>
             <h2>{name}</h2>
-
-            {todoType === "fixed" ? (<div>
-                <label>
-                    <p>{startTime} ~ {endTime}</p>
-                </label>
-            </div>) : todoType==="check" ? (
-                <div className={style.TodoCheck}>
-                    <label className={style.CheckboxLabel}>
+            {todoType === "fixed" ?
+                null : todoType === "check" ? (
+                    <div className={style.TodoCheck}>
+                        <label className={style.CheckboxLabel}>
+                            <input
+                                id="checkbox"
+                                type="checkbox"
+                                onChange={handleDone}
+                            />
+                            <span className={style.CheckboxCustom}></span>
+                        </label>
+                    </div>) : (
+                    <div className={style.TodoProgress}>
                         <input
-                            id="checkbox"
-                            type="checkbox"
-                            onChange={handleDone}
+                            type="range"
+                            min={0}
+                            max={100}
+                            step={10}
+                            value={sliderValue}
+                            onChange={handleSliderChange}
+                            style={{ background: `linear-gradient(to right, #3498db 0%, #3498db ${calculateFillPercentage()}%, #ecf0f1 ${calculateFillPercentage()}%, #ecf0f1 100%)` }}
                         />
-                        <span className={style.CheckboxCustom}></span>
-                    </label>
-                </div>) : (
-                <div className={style.TodoProgress}>
-                    <input
-                        type="range"
-                        min={0}
-                        max={100}
-                        step={10}
-                        value={sliderValue}
-                        onChange={handleSliderChange}
-                        style={{ background: `linear-gradient(to right, #3498db 0%, #3498db ${calculateFillPercentage()}%, #ecf0f1 ${calculateFillPercentage()}%, #ecf0f1 100%)` }}
-                    />
-                    <div>Progress: {sliderValue}/{100}</div>
-                </div>)}
+                        <div>Progress: {sliderValue}/{100}</div>
+                    </div>)}
         </div>
     )
 }
--- src/Presentation/Container/Main/index.tsx
+++ src/Presentation/Container/Main/index.tsx
@@ -1,12 +1,11 @@
 import { useState, ChangeEventHandler, useEffect } from 'react';
-import { Todo } from "@/Presentation/Component";
-import { getToday } from "@/Presentation/Component";
+import { Todo, getToday, showToday } from "@/Presentation/Component";
 //import { getAPIData } from '@/Data/DataSource';
 import todoData from '../../../tempData.json';
 import style from "@/Presentation/Style/Main.module.css";
 
 const Main = () => {
-
+    const upperBarDate = showToday();
     // isTodoCheck === false, make progress bar
     const [today, setToday] = useState("");
     useState(() => { setToday("231202"); }) // 이걸로 바꾸기 setToday(getToday());
@@ -25,6 +24,9 @@ const Main = () => {
     return (
         <div className={style.Main}>
             <div className={style.ContentBox}>
+                <div>
+                    <p>{upperBarDate}</p>
+                </div>
                 <div className={style.DayTodoBox}>
                 </div>
                 <div className={style.DayTimeLine}>
