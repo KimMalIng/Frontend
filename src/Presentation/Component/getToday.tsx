8b6fadf0c58f2fe06485cec343c0fc83c096945e
rockthepeople <3qufqks@gmail.com>
2023-12-05 02:03:49 +0900

    Update : todo디자인(1)

--- src/Presentation/Component/getToday.tsx
+++ src/Presentation/Component/getToday.tsx
@@ -1,8 +1,14 @@
-export default function getToday():string {
-    const today = new Date();    
-    const year = today.getFullYear();
-    const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
-    const day = today.getDate().toString().padStart(2, '0');
+const today = new Date();    
+const year = today.getFullYear();
+const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
+const day = today.getDate().toString().padStart(2, '0');
+
+export function getToday():string {
     const formattedDate:string = `${year}${month}${day}`;
+    return formattedDate;
+}
+
+export function showToday():string{
+    const formattedDate:string = `${month}월 ${day}일`;
     return formattedDate;
 }
