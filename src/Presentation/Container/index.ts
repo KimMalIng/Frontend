de3995ada728443664398b4c3e99c8d2b90dc43c
rockthepeople <3qufqks@gmail.com>
2023-12-05 22:31:17 +0900

    Upload - newtaskPage

--- src/Presentation/Container/index.ts
+++ src/Presentation/Container/index.ts
@@ -2,5 +2,6 @@ import Login from './Login';
 import Main from './Main';
 import Register from './Register';
 import Test from './Test';
+import NewTask from './NewTask';
 
-export { Login, Main, Register, Test };
+export { Login, Main, Register, Test, NewTask };
