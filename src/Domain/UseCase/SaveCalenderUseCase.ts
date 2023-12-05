da0eee06508455cc9f58a8d08dda8ee5a47ce87b
Jhun24 <jjanghuni102@naver.com>
2023-12-05 11:04:05 +0900

    refactor:

--- src/Domain/UseCase/SaveCalenderUseCase.ts
+++ src/Domain/UseCase/SaveCalenderUseCase.ts
@@ -7,10 +7,31 @@ class SaveCalenderUseCase {
     this.calenderReposiotry = cr;
   }
 
-  async execute(id: number, name: string, label: number, deadline: Date, estimated_time: number): Promise<void>{
+  async execute(
+    id: number | null | undefined, 
+    name: string | null | undefined, 
+    label: number | null | undefined, 
+    deadline: Date | null | undefined, 
+    estimatedTime: number | null | undefined
+  ): Promise<void>{
+    if(
+      typeof id === "undefined" ||
+      typeof name === "undefined" ||
+      typeof label === "undefined" ||
+      typeof deadline === "undefined" ||
+      typeof estimatedTime === "undefined" ||
+      id === null ||
+      name === null ||
+      label === null || 
+      deadline === null ||
+      estimatedTime === null
+    ){
+      return Promise.reject(400);
+    }
+    
     // 조건
     try {
-       await this.calenderReposiotry.saveCalender(id, name, label, deadline, estimated_time);
+       await this.calenderReposiotry.saveCalender(id, name, label, deadline, estimatedTime);
     } catch (error) {
       return Promise.reject(error);
     }
